2f078f6ec6216f7fa484e973fb8ea8c46d6a9ab0
pabloalmunia <pablo.almunia@corusconsulting.com>
2025-08-11 14:40:56 +0200

    Update case20 iframe to replace `g-composer` with `grapper-view` and adjust script/link references.

--- test/cases/case20.js
+++ test/cases/case20.js
@@ -2,6 +2,6 @@ export const title       = '20) iframe';
 export const description = `edit grapper-view from iframe`;
 
 export default `
-<iframe id="demo" src="/editor/test/assets/page.html" style="height: 300px; width: 90%;"></iframe>
+<iframe id="demo" src="/test/assets/page.html" style="height: 300px; width: 90%;"></iframe>
 <g-editor href="#demo" options="g-script: true"></g-editor>
 `;
